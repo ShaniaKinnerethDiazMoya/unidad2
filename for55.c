694fdb286c97415df02299fc18c63cb88435baf8
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-04-05 23:56:52 -0600

    Add files via upload

---
+++ for55.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Números del 10 al 1 en orden descendente:\n");
+    for(int i = 10; i >= 1; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
