81c857c2ec94eeccafca2301273497a1d03b534b
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-04-09 23:32:42 -0600

    Add files via upload

---
+++ if44.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 10;
+    
+    if (a > b) {
+        printf("a es mayor que b.\n");
+    } else {
+        printf("b es mayor o igual que a.\n");
+    }
+    
+    return 0;
+}
