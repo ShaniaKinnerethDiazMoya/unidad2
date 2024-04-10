4469ea59cfc299bfe2c64bb14958876be8c3ac9c
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-04-09 23:28:42 -0600

    Add files via upload

---
+++ if22.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 4;
+    
+    if (numero % 2 == 0) {
+        printf("%d es par.\n", numero);
+    } else {
+        printf("%d es impar.\n", numero);
+    }
+    
+    return 0;
+}
